9179d137f584dcce27fb8e433ff6a510931b576e
Craig McKeachie <craigdmckeachie@gmail.com>
2022-02-10 09:35:17 -0500

    components completed

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <a href="#">Privacy</a>
+        <a href="#">Terms</a>
+        <a href="#">Careers</a>
+        <span> &#169; Acme Inc.</span>
+      </nav>
+    </footer>
+  )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quam
+        mollitia ut, odit suscipit velit molestias, ratione vel minus iure esse!
+        Nam autem vitae distinctio minima facere. Consectetur, error molestiae.
+      </p>
+    </Layout>
+  )
+}
